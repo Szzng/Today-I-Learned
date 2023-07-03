63d95ba5db2602c5314aef5f50247f17d5652b30
Szzng <seungheeseo53@gmail.com>
2023-07-03 22:41:28 +0900

    Create balanced_binary_tree.py

---
+++ Tree/CodingTest/balanced_binary_tree.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/balanced-binary-tree/submissions/
+
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def depth(self, node):
+        if node is None: return 0
+
+        left = self.depth(node.left)
+        right = self.depth(node.right)
+
+        if abs(left - right) > 1: self.result = False
+
+        return max(left, right) + 1
+
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        self.result = True
+        self.depth(root)
+        return self.result
