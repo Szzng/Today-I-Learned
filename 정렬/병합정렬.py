33e65cfb6b79521e3253154f41175cdf26085aa2
Szzng <seungheeseo53@gmail.com>
2023-08-18 15:15:18 +0900

    add: 정렬 - 병합정렬

---
+++ 정렬/병합정렬.py
@@ -0,0 +1,48 @@
+# https://www.acmicpc.net/problem/2751
+
+"""
+병합정렬
+- 분할 정복 알고리즘의 하나
+- 시간복잡도 O(nlogn)
+"""
+
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+
+    merged = []
+    left_idx = 0
+    right_idx = 0
+    while left_idx < len(left) and right_idx < len(right):
+        if left[left_idx] < right[right_idx]:
+            merged.append(left[left_idx])
+            left_idx += 1
+        else:
+            merged.append(right[right_idx])
+            right_idx += 1
+
+    if left_idx < len(left):
+        merged += left[left_idx:]
+    elif right_idx < len(right):
+        merged += right[right_idx:]
+
+    return merged
+
+
+import sys
+
+read = sys.stdin.readline
+write = sys.stdout.write
+
+n = int(read())
+nums = [int(read()) for _ in range(n)]
+
+merged_nums = merge_sort(nums)
+
+write('\n'.join(map(str, merged_nums)))
