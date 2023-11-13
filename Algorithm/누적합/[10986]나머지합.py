fc8b25675e4250212e356ea69a7bcf457ea19e18
Szzng <seungheeseo53@gmail.com>
2023-11-13 19:33:12 +0900

    refactor

--- Algorithm/누적합/[10986]나머지합.py
+++ Algorithm/누적합/[10986]나머지합.py
@@ -1,7 +1,7 @@
 # https://www.acmicpc.net/problem/10986
 
 """
-sums[i]%M == sums[j]%M 이면, (sums[i] - sums[j])%M == 0 이므로, 나머지가  같은 구간의 개수를 구하면 된다.
+sums[i]%M == sums[j]%M 이면, (sums[i] - sums[j])%M == 0 이므로, 나머지가 같은 구간의 개수를 구하면 된다.
 
 나머지가 원래 0인 것은 당연히 무조건 포함되므로, answer에 미리 더해준다.
 """
@@ -12,15 +12,17 @@ import sys
 read = sys.stdin.readline
 
 n, m = map(int, read().split())
-numbers = list(map(int, read().split()))
-sums = [0] * len(numbers)
-answer = 0
+nums = list(map(int, read().split()))
+
+sums = [0] * n
 remain_dict = {}
+answer = 0
 
 for i in range(n):
-    sums[i] = sums[i - 1] + numbers[i] if i != 0 else numbers[i]
+    sums[i] = sums[i - 1] + nums[i] if i != 0 else nums[i]
 
     remain = sums[i] % m
+
     if remain == 0:
         answer += 1
 
