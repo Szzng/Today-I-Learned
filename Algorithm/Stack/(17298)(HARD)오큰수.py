0f667ecb367ef7822f3c5b46d84350f96bc93cd1
Szzng <seungheeseo53@gmail.com>
2023-11-22 11:40:42 +0900

    refactor

---
+++ Algorithm/Stack/(17298)(HARD)오큰수.py
@@ -0,0 +1,29 @@
+# https://www.acmicpc.net/problem/17298
+
+
+"""
+- 오큰수: 오른쪽에 있으면서 자신보다 큰 수 중에서 가장 왼쪽에 있는 수 (= 오른쪽에 있으면서 자신보다 큰 수 중에서 가장 가까운 수)
+- 다시 보고 연습 강력추천!!!
+
+- 스택의 기본 개념을 이해하고 적용하는 문제!
+- 스택의 후입선출이라는 독특한 성질이 종종 시간 복잡도를 줄이거나 특정한 문제의 조건을 손쉽게 해결하는 실마리가 될 때가 있습니다.
+- 문제에 접근할 때 혹시 스택을 이용하면 손쉽게 풀리지 않는지 한 번쯤 고민해 보세요.
+"""
+
+from sys import stdin
+
+read = stdin.readline
+
+n = int(read())
+nums = list(map(int, read().split()))
+stack = []
+result = [-1] * n
+
+for idx, num in enumerate(nums):
+    # 이렇게 하면 스택에는 현재 num보다 큰 수만 남게 된다.
+    while stack and stack[-1][1] < num:
+        result[stack.pop()[0]] = num
+
+    stack.append((idx, num))
+
+print(*result)
