6781114420acecddcb9873bc9915bed9c7591b74
Szzng <seungheeseo53@gmail.com>
2023-11-23 11:01:45 +0900

    rename, comment

--- Algorithm/Queue/(2164)카드.py
+++ Algorithm/Queue/(2164)카드.py
@@ -1,5 +1,12 @@
 # https://www.acmicpc.net/problem/2164
 
+"""
+시간 복잡도
+- 파이썬 프로그램에서는 1초에 2천만 번 ~ 1억 번의 연산 수행 가능
+- 1 ≤ N ≤ 500,000 / 2초
+- N이 500,000이므로 1초 안에 해결 가능
+"""
+
 from collections import deque
 
 n = int(input())
