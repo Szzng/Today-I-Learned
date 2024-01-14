f9baefa166f263937039932b3eedc9d8801d52e0
Szzng <seungheeseo53@gmail.com>
2024-01-14 14:40:43 +0900

    fix comment

--- Algorithm/정렬/(B2750)버블정렬.py
+++ Algorithm/정렬/(B2750)버블정렬.py
@@ -11,7 +11,7 @@
 n = int(input())
 nums = [int(input()) for _ in range(n)]
 
-for i in range(n - 1):  # j+1을 사용하므로 n-1까지만 반복
+for i in range(n - 1):
     for j in range(n - 1 - i):  # 반복을 할 때마다 맨 뒤부터 i개는 이미 정렬된 영역을 나타내므로, 반복할 필요가 없음.
         if nums[j] > nums[j + 1]:
             nums[j], nums[j + 1] = nums[j + 1], nums[j]
