cc08e3fa35b258c0d555a3fdbc30694d51793ca8
Szzng <seungheeseo53@gmail.com>
2024-01-23 23:05:09 +0900

    add: 최소공통조상_2의 거듭제곱 풀이

--- Algorithm/Tree/(B11438)최소공통조상_LCA_2거듭제곱.py
+++ Algorithm/Tree/(B11438)최소공통조상_LCA_2거듭제곱.py
@@ -1,10 +1,10 @@
-# https://www.acmicpc.net/problem/11437
+# https://www.acmicpc.net/problem/11438
 
 """
 # 문제
-N(2 ≤ N ≤ 50,000)개의 정점으로 이루어진 트리가 주어진다.
+N(2 ≤ N ≤ 100,000)개의 정점으로 이루어진 트리가 주어진다.
 트리의 각 정점은 1번부터 N번까지 번호가 매겨져 있으며, 루트는 1번이다.
-두 노드의 쌍 M(1 ≤ M ≤ 10,000)개가 주어졌을 때,
+두 노드의 쌍 M(1 ≤ M ≤ 100,000)개가 주어졌을 때,
 두 노드의 가장 가까운 공통 조상이 몇 번인지 출력한다.
 
 # 입력
@@ -16,15 +16,19 @@ N(2 ≤ N ≤ 50,000)개의 정점으로 이루어진 트리가 주어진다.
 # 출력
 M개의 줄에 차례대로 입력받은 두 정점의 가장 가까운 공통 조상을 출력한다.
 
-# 시간제한: 3초
+# 시간제한: 1.5초
 # 시간복잡도: O()
+
+# 풀이
+B11437번보다 N(노드의 개수)가 2배, M(쌍의 개수)가 10배 더 크지만 시간은 1/2로 줄어듦.
+-> 제곱수 형태를 이용한 빠르게 최소 공통 조상을 구하는 방법을 사용해야 함.
 """
 
 import sys
 from collections import deque
 
 
-class LCA:
+class PowersLCA:
     def __init__(self, num_nodes):
         self.root = 1
         self.num_nodes = num_nodes
@@ -35,7 +39,13 @@ class LCA:
             self.tree[a].append(b)
             self.tree[b].append(a)
 
-        self.parent = [0] * (self.num_nodes + 1)
+        temp = 1
+        self.k_max = 0
+        while temp <= num_nodes:
+            temp <<= 1
+            self.k_max += 1
+
+        self.parents = [[0] * (self.num_nodes + 1) for _ in range(self.k_max + 1)]
         self.depth = [0] * (self.num_nodes + 1)
 
         self.set_parent_and_depth()
@@ -53,26 +63,36 @@ class LCA:
                     q.append(next_node)
                     visited[next_node] = True
 
-                    self.parent[next_node] = curr_node
+                    self.parents[0][next_node] = curr_node
                     self.depth[next_node] = self.depth[curr_node] + 1
 
+        for k in range(1, self.k_max + 1):
+            for node in range(1, self.num_nodes + 1):
+                # 점화식으로 2의 거듭제곱 부모들 채우기
+                self.parents[k][node] = self.parents[k - 1][self.parents[k - 1][node]]
+
     def get_lca(self, node1, node2):
-        if self.depth[node1] < self.depth[node2]:
-            node1, node2 = node2, node1  # node1이 무조건 더 깊은 노드가 되도록
+        if self.depth[node1] > self.depth[node2]:
+            node1, node2 = node2, node1  # node2가 무조건 더 깊은 노드가 되도록
 
         # 두 노드의 "깊이"가 같아질 때까지 더 깊은 노드를 위로 올림
-        while self.depth[node1] != self.depth[node2]:
-            node1 = self.parent[node1]
+        for k in range(self.k_max, -1, -1):
+            if pow(2, k) <= self.depth[node2] - self.depth[node1]:
+                if self.depth[node1] <= self.depth[self.parents[k][node2]]:
+                    node2 = self.parents[k][node2]
 
-        # 두 노드가 같아질 때까지(두 노드가 같아지는 순간이 LCA) 두 노드를 동시에 위로 올림
-        while node1 != node2:
-            node1 = self.parent[node1]
-            node2 = self.parent[node2]
+        for k in range(self.k_max, -1, -1):
+            if node1 == node2:
+                break
 
-        return node1
+            if self.parents[k][node1] != self.parents[k][node2]:
+                node1 = self.parents[k][node1]
+                node2 = self.parents[k][node2]
+
+        return node1 if node1 == node2 else self.parents[0][node1]
 
 
-lca = LCA(int(sys.stdin.readline()))
+lca = PowersLCA(int(sys.stdin.readline()))
 
 M = int(sys.stdin.readline())
 memo = {}  # 같은 질문일 경우 재계산을 하지 않기 위해 딕셔너리 자료형 사용 -> 시간초과 해결 방법
